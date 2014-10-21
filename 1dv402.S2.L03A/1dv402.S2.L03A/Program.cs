f284e6c8f939d4700ac35b75ae3561b286fc4f6a
Lisa Westlund <lw222gu@student.lnu.se>
2014-10-21 20:37:33 +0200

    Skrivit metoden ViewMenu i Program.cs

--- 1dv402.S2.L03A/1dv402.S2.L03A/Program.cs
+++ 1dv402.S2.L03A/1dv402.S2.L03A/Program.cs
@@ -10,7 +10,7 @@ namespace _1dv402.S2.L03A
     {
         static void Main(string[] args)
         {
-
+            ViewMenu();
         }
 
         private static Shape CreateShape(ShapeType shapeType)
@@ -23,6 +23,18 @@ namespace _1dv402.S2.L03A
 
         private static void ViewMenu()
         {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("==============================");
+            Console.WriteLine("=                            =");
+            Console.WriteLine("=    Geometriska figurer     =");
+            Console.WriteLine("=                            =");
+            Console.WriteLine("==============================");
+            Console.ResetColor();
+            Console.WriteLine("0: Avsluta.");
+            Console.WriteLine("1: Ellips.");
+            Console.WriteLine("0: Rektangel.");
+            Console.WriteLine("==============================");
+            Console.WriteLine("Ange menyval [0-2]: ");
         }
 
         private static void ViewShapeDetail (Shape shape)
